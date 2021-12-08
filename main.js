b2ccaff70b932e12e996f7af0f388b55fb65ae3a
hari1005 <62102842+hari1005@users.noreply.github.com>
2021-12-08 14:06:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+let latitude, longitude, destination;
+var img1=querySelector("#taj-mahal")
+var img2=querySelector("#Red-fort")
+var img3=querySelector("#goldonda-fort")
+$(document).ready(function () {
+    alert("Please allow the device to know your location!")
+    initGeolocation();
+})
+
+$(function () {
+    $("#navigate-button").click(function () {
+        window.location.href = `ar_navigation.html?source=${latitude};${longitude}&destination=${destination.lat};${destination.lng}`
+    })
+})
+
+function initGeolocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success);
+    }
+    else {
+        alert("Sorry, your browser does not support geolocation services.");
+    }
+}
+
+function success(position) {
+    longitude = position.coords.longitude;
+    latitude = position.coords.latitude
+
+    // Initializing Mapbox
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA';
+
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [longitude, latitude],
+        zoom: 16
+    });
+
+    map.addControl(
+        new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+        })
+    );
+
+    map.addControl(
+        new MapboxDirections({
+            accessToken: mapboxgl.accessToken
+        }),
+        'top-left'
+    );
+
+    map.on('click', function (e) {
+        destination = e.lngLat;
+    });
+}
+
+var marker1=new mapboxgl.Marker({
+    element:img1
+})
+.setlnglat(78.042068,27.173891)
+.addTo(map);
+var marker2=new mapboxgl.Marker({
+    element:img2
+})
+.setlnglat(77.2410,28.6562)
+.addTo(map)
+var marker3=new mapboxgl.Marker({
+    element:img3
+})
+.setlnglat(78.401604,17.382330)
+.addTo(map)
